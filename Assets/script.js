7152e08f3007ef3a48e145859f9d05cd694ffe17
Tom <tronglongphung@gmail.com>
2021-03-18 18:31:04 +1030

    jack fixed some of my logic

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specials = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numbers = '0123456789';
+var specials = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+var lowercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+var uppercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+function generatePassword() {
+  var passwordLengthRaw = prompt("How many characters do you want in a password?", "From 8 to 128 characters")
+  var passwordLength = parseInt(passwordLengthRaw);
+  if (!passwordLengthRaw || passwordLength < 8 || passwordLength > 128 ) {
+    alert('Password length not correct');
+  }
+  
+
+
+  for (let index = 0; index < passwordLength; index++) {
+    // make character set array from accepted characters
+    // calculate random index of full array 
+    
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
-  // if writePassword = true
-  if (confirm("Do you want to generate a password?")){ 
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    var passwordLength = prompt("How many characters do you want in a password?", "From 8 to 128 characters")
-   
-    
-
-  }
-  // if writePasword = false
-  else {
-    return
+  if (!confirm("Do you want to generate a password?")){ 
+    return;
   }
 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   
