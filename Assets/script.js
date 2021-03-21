f1fd45d17e0381d62a9131d875845a4c3330e7d8
Tom <tronglongphung@gmail.com>
2021-03-21 15:22:07 +1030

    questions worked out correctly, working on loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specials = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-var lowercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
-var uppercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+const specials = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+const lowercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+const uppercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+var passwordSelection = ""
+
 
 function generatePassword() {
-  var passwordLengthRaw = prompt("How many characters do you want in a password?", "From 8 to 128 characters")
+  var passwordLengthRaw = prompt("How many characters do you want in a password (8 - 128)?", "")
   var passwordLength = parseInt(passwordLengthRaw);
   if (!passwordLengthRaw || passwordLength < 8 || passwordLength > 128 ) {
-    alert('Password length not correct');
-  }
+    return alert('Password length not correct');
+  } 
   
+// console logs each line to see if the code is working correctly.
+  if (confirm("Do you want special characters")){
+    passwordSelection = specials;
+    console.log(passwordSelection)
+  } else {
+    passwordSelection = passwordSelection;
+  }
 
+  if(confirm("Do you want lowercase characters")){
+    passwordSelection = passwordSelection.concat(lowercase);
+    console.log(passwordSelection)
+  } else {
+    passwordSelection = passwordSelection;
+    console.log(passwordSelection)
+  }
 
+  if(confirm("Do you want uppercase characters")){
+    passwordSelection = passwordSelection.concat(uppercase);
+    console.log(passwordSelection)
+  } else {
+    passwordSelection = passwordSelection;
+    console.log(passwordSelection)
+  }
+
+  if (confirm("Do you want numbers")){
+    passwordSelection = passwordSelection.concat(numbers);
+    console.log(passwordSelection)
+  } else {
+  passwordSelection = passwordSelection;
+  console.log(passwordSelection)
+  }
+
+// this is a loop
   for (let index = 0; index < passwordLength; index++) {
     // make character set array from accepted characters
     // calculate random index of full array 
-    
+    [Math.floor(Math.random() * passwordSelection)]
+    return passwordSelection
   }
-
 }
+
 
 // Write password to the #password input
 function writePassword() {
